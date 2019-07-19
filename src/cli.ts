fdc25280787dc94f0ed0697fb267b277e1d413e1
Paul Melnikow <email@paulmelnikow.com>
2019-07-19 14:37:45 -0500

    Ignore lint

--- src/cli.ts
+++ src/cli.ts
@@ -6,6 +6,7 @@ import { ArgumentParser } from 'argparse'
 import Manifest, { MatchResult } from './manifest'
 import Archive from './archive'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { version } = require('../package.json')
 
 export default async function main(inArgs?: string[]) {
